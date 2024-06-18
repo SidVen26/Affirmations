ad5e25dff653d15f85e34bdd621a5a635770c2a4
Siddhi Venkat <143101520+SidVen26@users.noreply.github.com>
2024-06-18 17:49:41 -0500

    new file:   index.css 	new file:   index.html 	new file:   index.js 	new file:   snoopy.JPG

---
+++ index.js
@@ -0,0 +1,70 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+    databaseURL: "https://positivity-cb461-default-rtdb.firebaseio.com/"
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const messagesInDB = ref(database, "messageList");
+
+const subBtn = document.getElementById("submit-btn");
+const inputFieldEl = document.getElementById("input-el");
+const messageListEl = document.getElementById("ul-el");
+
+subBtn.addEventListener("click", function() {
+    let inputVal = inputFieldEl.value;
+    push(messagesInDB, inputVal);
+    clearInputField();
+});
+
+onValue(messagesInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let messageArray = Object.entries(snapshot.val());
+
+        clearMessageListEl();
+
+        for (let i = 0; i < messageArray.length; i++) {
+            let currentMessage = messageArray[i];
+            appendItemToMessageListEl(currentMessage);
+        }
+    } else {
+        messageListEl.innerHTML = "No messages yet";
+    }
+});
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+
+function appendItemToMessageListEl(item) {
+    let itemID = item[0];
+    let itemValue = item[1];
+
+    let newEl = document.createElement("li");
+
+    newEl.textContent = itemValue;
+
+    newEl.style.backgroundColor = getRandomColor()
+
+
+
+    messageListEl.append(newEl);
+}
+
+function clearInputField() {
+    inputFieldEl.value = "";
+}
+
+function clearMessageListEl() {
+    messageListEl.innerHTML = "";
+}
