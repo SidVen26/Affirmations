ecb9057378eae06d3f14024d58bc0d82f634e021
Siddhi Venkat <143101520+SidVen26@users.noreply.github.com>
2024-06-19 10:39:13 -0500

    modified:   index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ onValue(messagesInDB, function(snapshot) {
 });
 
 function getRandomColor() {
-    const letters = '0123456789ABCDEF';
+    const letters = '23456789ABCDEF';
     let color = '#';
     for (let i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
@@ -55,9 +55,7 @@ function appendItemToMessageListEl(item) {
     newEl.textContent = itemValue;
 
     newEl.style.backgroundColor = getRandomColor()
-
-
-
+    
     messageListEl.append(newEl);
 }
 
